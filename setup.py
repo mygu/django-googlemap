16f5f302dfe1563bf9ad240a83ba763d41a5d6b4
mygu <mingyugu0410@gmail.com>
2014-07-07 15:03:13 +0800

    update version to 0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-googlemap',
-    version='0.3',
+    version='0.4',
     packages=['googlemap'],
     include_package_data=True,
     license='BSD License',
